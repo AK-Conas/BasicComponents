ec00acb359054b1a510bc31747ae4713ae0edfb0
Akanksha <135117691+AK-Conas@users.noreply.github.com>
2023-09-24 16:16:44 +0000

    Update CButtonGroup.jsx

--- src/CButtonGroup.jsx
+++ src/CButtonGroup.jsx
@@ -2,11 +2,13 @@ import * as React from "react";
 import ButtonGroup from "@mui/material/ButtonGroup";
 import CButton from "./CButton";
 
-export default function CButtonGroup() {
+const CButtonGroup = ({}) => {
   return (
     <ButtonGroup>
       <CButton cMarginRight={true} cText="One" />
       <CButton cText="Two" />
     </ButtonGroup>
   );
-}
+};
+
+export default CButtonGroup;
